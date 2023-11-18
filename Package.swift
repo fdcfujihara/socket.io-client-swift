6081a01b01635c5166d1ec3f07071d8867f5e470
fuji <fdc.fuji@gmail.com>
2023-11-18 14:04:04 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "SocketIO"
+    name: "socket.io-client-swift",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "socket.io-client-swift",
+            targets: ["socket.io-client-swift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "socket.io-client-swift"),
+        .testTarget(
+            name: "socket.io-client-swiftTests",
+            dependencies: ["socket.io-client-swift"]),
+    ]
 )
---
+++ Tests/socket.io-client-swiftTests/socket_io_client_swiftTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import socket_io_client_swift
+
+final class socket_io_client_swiftTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
